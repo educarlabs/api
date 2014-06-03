488d7439161d1a81f1ad197bd243be89a510d177
Julian Paredes <jparedes@educ.gov.ar>
2014-06-03 15:36:49 -0300

    Ejemplos PHP

---
+++ php/noticias_recientes.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * API Publica educ.ar
+ *  
+ * Ejemplo de llamado a endpoint: Noticias Recientes
+ * 
+ */
+$service_url = 'https://api.educ.ar/0.9/noticias/recientes/';  
+$api_key = 'xxxxxxxxxxxxxxxxxxx';  //key
+$ssl_crt = getcwd().'/api_server.crt';  //cert
+$params = array('apikey'=>$api_key);
+
+if (!function_exists('curl_version'))
+    die ('Se necesita tener instalada la extensión cURL');
+
+$curl = curl_init($service_url.'?'.http_build_query($params));
+
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+// Debug
+//curl_setopt($curl, CURLOPT_VERBOSE, true);
+
+curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,false);
+curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,true);
+curl_setopt($curl,CURLOPT_CAINFO,$ssl_crt);
+
+$curl_response = curl_exec($curl);
+
+if ($curl_response === false) {
+    $info = curl_getinfo($curl);
+    curl_close($curl);
+    die('Ocurrió un error en la ejecución de llamada curl. Info adicional: ' . var_export($info,true));
+}
+curl_close($curl);
+
+
+$decoded = json_decode($curl_response);
+if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
+    die('Ocurrió un error: ' . $decoded->response->errormessage);
+}
+
+echo 'Respuesta ok!';
+var_export($decoded->result->data);
+?>
